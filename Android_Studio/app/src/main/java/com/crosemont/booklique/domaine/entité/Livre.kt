706bb5eb00992223441725510dc374c1b43efe00
Handy Charles G. Pierre <hpierre@dept-info.crosemont.quebec>
2024-11-05 18:13:52 -0500

    Création de la class Livre

---
+++ Android_Studio/app/src/main/java/com/crosemont/booklique/domaine/entité/Livre.kt
@@ -0,0 +1,16 @@
+package com.crosemont.booklique.domaine.entité
+
+import java.util.Date
+
+class Livre(
+    val isbn : String,
+    val titre : String,
+    val description : String,
+    val auteur : String,
+    val editeur : String,
+    val genre : String,
+    date_publication : Date,
+    nombre_pages : Int,
+    quantite : Int,
+    ) {
+}
