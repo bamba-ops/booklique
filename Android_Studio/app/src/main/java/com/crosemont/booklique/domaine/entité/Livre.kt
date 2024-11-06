9adc37bedcd7e1d088aaa78bef1c0c6b96c6007e
Handy Charles G. Pierre <hpierre@dept-info.crosemont.quebec>
2024-11-06 06:04:04 -0500

    Faire en sorte que la classe Livre implémente Parcelable pour  être Parcelable de passée dans un Bundle

--- Android_Studio/app/src/main/java/com/crosemont/booklique/domaine/entité/Livre.kt
+++ Android_Studio/app/src/main/java/com/crosemont/booklique/domaine/entité/Livre.kt
@@ -1,17 +1,22 @@
-package com.crosemont.booklique.domaine.entité
-
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.Date
 
-class Livre(
-    val isbn : String,
-    val image_url : String,
-    val titre : String,
-    val description : String,
-    val auteur : String,
-    val editeur : String,
-    val genre : String,
-    date_publication : Date,
-    nombre_pages : Int,
-    quantite : Int,
-    ) {
+@Parcelize
+data class Livre(
+    val isbn: String,
+    val image_url: String,
+    val titre: String,
+    val description: String,
+    val auteur: String,
+    val editeur: String,
+    val genre: String,
+    val date_publication: Date,
+    val nombre_pages: Int,
+    val quantite: Int
+) : Parcelable {
+
+    fun estDisponible(): Boolean {
+        return quantite > 0
+    }
 }
