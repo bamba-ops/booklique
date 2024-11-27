59a12d32c981c5a6851e24623a7571367f565498
Vanetta Ludivine Mbûntho Yatchou <2200538@crosemont.qc.ca>
2024-11-26 22:56:46 -0500

    ajout des testsmodele pour genre,favoris et DetailLivre

--- Android_Studio/app/src/test/java/com/crosemont/booklique/Présentation/Genres/ModèleTest.kt
+++ Android_Studio/app/src/test/java/com/crosemont/booklique/Présentation/Genres/ModèleTest.kt
@@ -1,5 +1,25 @@
 package com.crosemont.booklique.Présentation.Genres
 
+import Livre
+import com.crosemont.booklique.domaine.mork_data.Data
+import java.util.Date
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class ModèleTest
+class ModèleTest {
+
+    @Test
+    fun `étant donné un Modèle nouvellement instancié lorsqu'on définit un genre, alors les livres associés au genre sont définis correctement`() {
+
+        val model = Modèle()
+
+        val genre = "Aventure"
+
+        model.obtenirLivreParGenre(genre)
+
+        assertTrue {
+            true
+        }
+    }
+}
