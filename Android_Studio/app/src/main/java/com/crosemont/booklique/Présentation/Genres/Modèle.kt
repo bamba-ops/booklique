48f2c836289c85c0c54ea6bc67e7b45a51ac74df
Elogiste <elogeassiobo@gmail.com>
2024-12-11 15:33:03 -0500

    Correction Genres/Modèle

--- Android_Studio/app/src/main/java/com/crosemont/booklique/Présentation/Genres/Modèle.kt
+++ Android_Studio/app/src/main/java/com/crosemont/booklique/Présentation/Genres/Modèle.kt
@@ -1,20 +1,10 @@
 package com.crosemont.booklique.Présentation.Genres
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.net.ConnectivityManager
 import com.crosemont.booklique.domaine.service.LivreService
 
 class Modèle {
 
     fun obtenirLivreParGenre(genre: String){
         LivreService.definirLivreParGenre(genre)
-    }
-
-    @SuppressLint("ServiceCast")
-    fun connexion(context : Context) : Boolean{
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
