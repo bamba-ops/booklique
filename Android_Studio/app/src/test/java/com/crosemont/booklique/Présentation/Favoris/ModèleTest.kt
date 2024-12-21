2e25744732838b1f872a78130fb38f2a3a644955
Vanetta Ludivine Mbûntho Yatchou <2200538@crosemont.qc.ca>
2024-12-20 22:52:08 -0500

    ajout du code pour le test modele

--- Android_Studio/app/src/test/java/com/crosemont/booklique/Présentation/Favoris/ModèleTest.kt
+++ Android_Studio/app/src/test/java/com/crosemont/booklique/Présentation/Favoris/ModèleTest.kt
@@ -15,8 +15,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
-@ExperimentalCoroutinesApi
 class ModèleTest {
 
 }
